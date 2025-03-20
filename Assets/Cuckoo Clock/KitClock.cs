9d79d890b9195ea71263b7405f5afe2dc3bd6399
Grrrrretz <lhj15733527022@gmail.com>
2025-03-20 14:15:52 -0400

    week10 exercise

---
+++ Assets/Cuckoo Clock/KitClock.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KitClock : MonoBehaviour
+{
+    public Transform hourHand;
+    public Transform MinuteHand;
+    public float timeAnHourTakes = 5;
+
+    public float t;
+    public int hour = 0;
+
+    public UnityEvent<int> OnTheHour;
+
+    Coroutine clockIsRunning;
+    IEnumerator doOneHour;
+
+    void Start()
+    {
+       clockIsRunning =  StartCoroutine(MoveThenClock());
+    }
+
+    IEnumerator MoveThenClock()
+    {
+        while (true)
+        {
+            doOneHour = MoveTheClockHandOneHour();
+            yield return  StartCoroutine(doOneHour);
+        }
+    }
+ 
+
+    IEnumerator MoveTheClockHandOneHour()
+    {
+        t = 0;
+        while(t < timeAnHourTakes)
+        {
+            t += Time.deltaTime;
+            MinuteHand.Rotate(0, 0, -(360 / timeAnHourTakes)*Time.deltaTime);
+            hourHand.Rotate(0, 0, -(30 / timeAnHourTakes) * Time.deltaTime);
+            yield return null;
+        }
+        hour++;
+        if(hour == 13)
+        {
+            hour = 1;
+        }
+        OnTheHour.Invoke(hour);
+    }
+    public void stopTheClock()
+    {
+        if(clockIsRunning != null)
+        {
+            StopCoroutine(clockIsRunning);
+        }
+        if(doOneHour != null)
+        {
+            StopCoroutine(doOneHour);
+        }
+      
+    }
+}
