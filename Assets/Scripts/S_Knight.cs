854161bab226b949adf0603f9893d4f260880c22
Grrrrretz <lhj15733527022@gmail.com>
2025-03-27 14:02:43 -0400

    finish kinght animation

---
+++ Assets/Scripts/S_Knight.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Knight : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Animator animator;
+    public float speed = 2;
+    public bool canrun = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+
+        sr.flipX = direction < 0;
+
+        animator.SetFloat("Movement", Mathf.Abs(direction));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+            canrun = false;
+        }
+
+        if(canrun = true)
+        {
+            transform.position += transform.right * direction * speed * Time.deltaTime;
+        }
+
+    }
+
+    public void AttackHasFinish()
+    {
+        Debug.Log("attack is finished");
+        canrun = true;
+    }
+}
